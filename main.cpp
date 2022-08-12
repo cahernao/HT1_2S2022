e486b8def5c691103bccccea94f78090c05bbda7
Carlos Hernández <58708426+chno2016@users.noreply.github.com>
2022-08-11 21:58:35 -0600

    nombre de autor

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ fun2 *funcion = new fun2();
 int main(int argc, char *argv[])
 {
 
+    cout<<"         CARNET: 201611269 NOMBRE: CARLOS AUGUSTO HERNÁNDEZ ORDOÑEZ";
     funcion->menu();
     return 0;
 }
